af45da8c1ac60d2ef977077b6fdf20f9b0ace8a4
public_hamlet <anooj4trade@gmail.com>
2022-09-15 12:52:56 +0530

    Rectify the error in 78th Movie

--- main.py
+++ main.py
@@ -29,70 +29,77 @@ imdb_url = 'https://www.imdb.com/chart/top/'
 imdb_req = requests.get(imdb_url)
 print(f'Website Status code: {imdb_req.status_code}')
 
-imdb_html = imdb_req.text
+if imdb_req.status_code == 200:
 
-""" Parser through the website using BS4 """
-imdb_soup = BeautifulSoup(imdb_html, 'html.parser')
-
-""" Checking the object imdb_soup """
-print(imdb_soup.title.string == 'Top 250 Movies - IMDb')
-
-""" Creating the csv file """
-if csv_file not in os.listdir('downloads'):
-    with open(csv_file_path, 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([
-            'Movie Title', 'Release Year', 'Storyline', 'Movie Genre',
-            'Movie Duration', 'IMDb Movie Rating', 'Movie Certification',
-            'IMDb Users Votes'])
-
-""" The details required are under section 
-        <tbody class='lister-list'>
-            <tr> """
-
-moviePage_list = list()
-
-tbody_sec = imdb_soup.find(name='tbody', attrs={'class': 'lister-list'})
-tr_secs = tbody_sec.findAll(name='tr')
-
-count = 1
-
-for tr_sec in tr_secs:
-    os.system('clear')
-    print(f'processing page {count} .............')
+    imdb_html = imdb_req.text
     
-    td_movNameYear = tr_sec.find(name='td', attrs={'class': 'titleColumn'})
-    movieTitle = td_movNameYear.a.string
-    movieYear = td_movNameYear.span.string[1:-1]
+    """ Parser through the website using BS4 """
+    imdb_soup = BeautifulSoup(imdb_html, 'html.parser')
     
-    # if statement for avaoiding the repeated movie details
-    if (movieTitle, movieYear) not in moviePage_list: 
-        moviePage_list.append((movieTitle, movieYear))
-        
-        moviePage_url = 'https://imdb.com' + td_movNameYear.a['href']
-        moviePage_obj = MovieInnerPage(user_url=moviePage_url)
-        
-        movieStory = moviePage_obj.movieStory()
-        movieCategories = moviePage_obj.movieCategories()
-        movieHours = moviePage_obj.movieHours()
-        movieRating = moviePage_obj.movieRating()
-        movieCert = moviePage_obj.movieCert()
-        movieVote = moviePage_obj.movieVote()
-        
-        movieRow = [movieTitle, movieYear, movieStory, movieCategories,
-                        movieHours, movieRating, movieCert, movieVote]
-        
-        with open(csv_file_path, 'a') as csvfile:
+    """ Checking the object imdb_soup """
+    print(imdb_soup.title.string == 'Top 250 Movies - IMDb')
+    
+    """ Creating the csv file """
+    if csv_file not in os.listdir('downloads'):
+        with open(csv_file_path, 'w') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(movieRow)
+            writer.writerow([
+                'Movie Title', 'Release Year', 'Storyline', 'Movie Genre',
+                'Movie Duration', 'IMDb Movie Rating', 'Movie Certification',
+                'IMDb Users Votes'])
+    
+    """ The details required are under section 
+            <tbody class='lister-list'>
+                <tr> """
+    
+    moviePage_list = list()
+    
+    tbody_sec = imdb_soup.find(name='tbody', attrs={'class': 'lister-list'})
+    tr_secs = tbody_sec.findAll(name='tr')
+    
+    count = 1
+    
+    for tr_sec in tr_secs:
+        os.system('clear')
+        print(f'processing page {count} .............')
         
-        moviePage_obj.htmlGenerate()
-        count += 1
+        td_movNameYear = tr_sec.find(name='td', attrs={'class': 'titleColumn'})
+        movieTitle = td_movNameYear.a.string
+        movieYear = td_movNameYear.span.string[1:-1]
+        
+        # if statement for avaoiding the repeated movie details
+        if (movieTitle, movieYear) not in moviePage_list: 
+            moviePage_list.append((movieTitle, movieYear))
+            
+            moviePage_url = 'https://imdb.com' + td_movNameYear.a['href']
+            moviePage_obj = MovieInnerPage(user_url=moviePage_url)
+            
+            movieStory = moviePage_obj.movieStory()
+            movieCategories = moviePage_obj.movieCategories()
+            movieHours = moviePage_obj.movieHours()
+            movieRating = moviePage_obj.movieRating()
+            movieCert = moviePage_obj.movieCert()
+            movieVote = moviePage_obj.movieVote()
+            
+            movieRow = [movieTitle, movieYear, movieStory, movieCategories,
+                            movieHours, movieRating, movieCert, movieVote]
+            
+            with open(csv_file_path, 'a') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(movieRow)
+            
+            moviePage_obj.htmlGenerate()
+            count += 1
+    
+        else:
+            continue
+        
+    print(
+        f'Total time taken to complete the scrapping \
+            IMDb website using BS4 & Requests \
+                modules{(perf_counter() - start) / 60:.4f}min')
 
-    else:
-        continue
+else:
+    print('https connection error.....................................')
+    print('please restart the program after sometime.....................................')
 
-print(
-    f'Total time taken to complete the scrapping \
-        IMDb website using BS4 & Requests \
-            modules{(perf_counter() - start) / 60:.4f}min')
